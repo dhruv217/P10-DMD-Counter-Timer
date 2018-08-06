36a6b906f8311c0b35eb232f39bc0501a91a8e0d
Dhruv Tripathi <dhruv.tru7@gmail.com>
2018-08-06 12:49:35 +0530

    Second Commit

--- P10-DMD-Counter-Timer.ino
+++ P10-DMD-Counter-Timer.ino
@@ -15,14 +15,14 @@
 #define SET_TIME_MM 1
 #define SET_TIME_SS 2
 
-boolean counting = true;
+boolean counting = false;
 int Mode = COUNTER;
 
 /* Pin Comfig */
 int buttonStartStop = 14;
 int buttonSetMode = 15;
 int buttonTimeUp = 16;
-int time[4];
+int time[4], i;
 
 
 DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
@@ -35,7 +35,6 @@ void ScanDMD() {
 
 void DecrementTime() {
 	if((counting) && ((time[0] != 0) || (time[1] != 0) || (time[2] != 0) || (time[3] != 0))) {
-		Serial.println("decrementing");
 		if(time[3] == 0) {
 			time[3] = 9;
 			if(time[2] == 0) {
@@ -66,7 +65,8 @@ void setup(){
 	MsTimer2::set(1000, DecrementTime); // 1000ms period
   	MsTimer2::start();
 	dmd.clearScreen( true );
-	time[0] = 1;
+	dmd.selectFont(Arial_Black_16);
+	time[0] = 0;
 	time[1] = 0;
 	time[2] = 0;
 	time[3] = 0;
@@ -74,7 +74,8 @@ void setup(){
 }
 
 void loop() {
-	if (digitalRead(buttonSetMode) == LOW){
+	if (digitalRead(buttonSetMode) == LOW) {
+		Serial.println("Mode Changed To "+ Mode);
 		if(Mode == SET_TIME_SS){
 			Mode = COUNTER;
 		} else {
@@ -83,26 +84,36 @@ void loop() {
     }
 	if (digitalRead(buttonStartStop) == LOW && Mode == COUNTER) {
 		counting = !counting;
+		if(counting)Serial.println("Start");
+		else Serial.println("Stop"); 
 	}
     switch (Mode) {
 		case COUNTER: 
 			Timer();
 			break;
 		case SET_TIME_MM:
+			counting = false;
 			DisplaySetTimeMM();
 			break;
 		case SET_TIME_SS:
-			DisplaySetTimeMM();
+			counting = false;
+			DisplaySetTimeSS();
 			break;
 	}
-	delay(100);
+	if (Mode != COUNTER)
+	{
+		i += 1;
+		if (i == 59999)
+			i = 0;
+	}
+	delay(333);
 }
 
 void Timer() {
 	printTimeToLED();
 }
 
-void DisplaySetTimeMM(void)
+void DisplaySetTimeMM()
 {
     if (digitalRead(buttonTimeUp) == LOW) {    // Increment
         if (time[0] == 5 && time[1] == 9) {
@@ -117,13 +128,61 @@ void DisplaySetTimeMM(void)
             time[1] += 1;
         }
     }
-    printTimeToLED();
+    printTimeToLEDMM();
+}
+
+void DisplaySetTimeSS()
+{
+	if (digitalRead(buttonTimeUp) == LOW)
+	{ // Increment
+		if (time[2] == 5 && time[3] == 9)
+		{
+			time[2] = 0;
+			time[3] = 0;
+		}
+		else if (time[3] == 9)
+		{
+			time[2] += 1;
+			time[3] = 0;
+		}
+		else
+		{
+			time[3] += 1;
+		}
+	}
+	printTimeToLEDSS();
 }
 
 void printTimeToLED() {
-	str="";
 	str = String(time[0])+String(time[1])+":"+String(time[2])+String(time[3]);
 	str.toCharArray(b,6);
-	dmd.selectFont(Arial_Black_16);
 	dmd.drawString( 0, 1, b, 5, GRAPHICS_NORMAL );
+}
+
+void printTimeToLEDMM()
+{
+	if(i % 2 == 0) {
+	str = String(time[0]) + String(time[1]) + ":" + String(time[2]) + String(time[3]);
+	str.toCharArray(b, 6);
+	dmd.drawString(0, 1, b, 5, GRAPHICS_NORMAL);
+	} else {
+		str = "  :" + String(time[2]) + String(time[3]);
+		str.toCharArray(b, 6);
+		dmd.drawString(0, 1, b, 5, GRAPHICS_NORMAL);
+	}
+}
+void printTimeToLEDSS()
+{
+	if (i % 2 == 0)
+	{
+		str = String(time[0]) + String(time[1]) + ":" + String(time[2]) + String(time[3]);
+		str.toCharArray(b, 6);
+		dmd.drawString(0, 1, b, 5, GRAPHICS_NORMAL);
+	}
+	else
+	{
+		str = String(time[0]) + String(time[1]) + ":  ";
+		str.toCharArray(b, 6);
+		dmd.drawString(0, 1, b, 5, GRAPHICS_NORMAL);
+	}
 }
